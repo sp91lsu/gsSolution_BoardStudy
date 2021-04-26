192f7da8a3db779b768e9818393f6000aaffec35
이성웅 <dev.lsw91@gmail.com>
2021-04-26 21:10:28 +0900

    BoardDaoImpl.java 코드 더 깔끔하게 보수

--- src/main/java/com/study/happy/dao/impl/BoardDaoImpl.java
+++ src/main/java/com/study/happy/dao/impl/BoardDaoImpl.java
@@ -1,9 +1,7 @@
 package com.study.happy.dao.impl;
 
 import com.study.happy.dao.BoardDao;
-import org.apache.ibatis.session.SqlSession;
 import org.mybatis.spring.SqlSessionTemplate;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -16,44 +14,48 @@ public class  BoardDaoImpl implements BoardDao {
     @Autowired
     private SqlSessionTemplate sqlSession;
 
+    public BoardDao mapper(){
+        return sqlSession.getMapper(BoardDao.class);
+    }
+
     @Override
     public List<Map<String, Object>> list() {
-        return sqlSession.getMapper(BoardDao.class).list();
+        return mapper().list();
     }
 
     @Override
     public int write(Map<String, Object> map) {
-        return sqlSession.getMapper(BoardDao.class).write(map);
+        return mapper().write(map);
     }
 
     @Override
     public Map<String, Object> one(int seqInt) {
-        return sqlSession.getMapper(BoardDao.class).one(seqInt);
+        return mapper().one(seqInt);
     }
 
     @Override
     public void viewCntInc(int seqInt) {
-        sqlSession.getMapper(BoardDao.class).viewCntInc(seqInt);
+        mapper().viewCntInc(seqInt);
     }
 
     @Override
     public Map<String, Object> miniOne(int seqInt) {
-        return sqlSession.getMapper(BoardDao.class).miniOne(seqInt);
+        return mapper().miniOne(seqInt);
     }
 
     @Override
     public int update(Map<String, Object> map) {
-        return sqlSession.getMapper(BoardDao.class).update(map);
+        return mapper().update(map);
     }
 
     @Override
     public int delete(List<String> seqList) {
-        return sqlSession.getMapper(BoardDao.class).delete(seqList);
+        return mapper().delete(seqList);
     }
 
     @Override
     public List<Map<String, Object>> search(Map<String, String> schInfo) {
-        return sqlSession.getMapper(BoardDao.class).search(schInfo);
+        return mapper().search(schInfo);
     }
 
 }
